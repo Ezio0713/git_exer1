8c9a4e625ef198c5310097cc6042f106416644b4
qzx <qzx@qq.com>
2022-07-04 14:52:44 +0800

    远程分支3

--- src/main/java/Gitexerce.java
+++ src/main/java/Gitexerce.java
@@ -2,5 +2,6 @@ public class Gitexerce {
     public static void main(String[] args) {
         System.out.println("version1");
         System.out.println("version2");
+        System.out.println("master3");
     }
 }
